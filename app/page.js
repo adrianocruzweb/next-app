cc40b29654d7c3708635a9589642a0ebf8a85f4f
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-10-08 18:14:04 -0300

    auth succes

--- app/page.js
+++ app/page.js
@@ -1,8 +1,24 @@
-export default function Home() {
+// app/page.js (ou outro arquivo dentro de app/)
+"use client";
 
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+
+  if (session) {
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <>
+        <h1>Olá, {session.user.name}</h1>
+        <button onClick={() => signOut()}>Sair</button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Você não está autenticado</h1>
+        <button onClick={() => signIn()}>Entrar</button>
+      </>
     );
   }
+}
